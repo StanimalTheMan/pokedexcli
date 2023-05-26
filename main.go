50f0ada361a0c87bf1946d33a2a2439cf621ee2a
StanimalTheMan <sjc591@nyu.edu>
2023-05-26 19:45:53 -0400

    Try to put everything in main package, store commands in map, build repl

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name	    string
+	description string
+	callback    func() error
+}
+
+func commandHelp () error {
+	fmt.Println("Welcome to the Pokedex!")
+}
+
+func commandExit() error {
+	return nil
+}
+
+func getCliCommand() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:	     "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		userInput := scanner.Text()
+		if userInput == "help" {
+			getCliCommand()["help"]
+		} else if userInput == "exit" {
+			getCliCommand()["exit"]
+		}
+	}
 }
