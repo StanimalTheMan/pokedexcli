46276d82699814cbbf4aa5818b3bc7c11deb729e
Stan Choi <sjc591@nyu.edu>
2025-01-13 10:07:57 -0500

    Init basic repl read

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		lowerCaseUserInput := strings.ToLower(userInput)
+		words := strings.Fields(lowerCaseUserInput)
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
