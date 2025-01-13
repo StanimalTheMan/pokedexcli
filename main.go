7e91da7373d567c635f52206ea0e9194bc62dbad
Stan Choi <sjc591@nyu.edu>
2025-01-13 13:29:06 -0500

    Refactor code

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		userInput := scanner.Text()
-		lowerCaseUserInput := strings.ToLower(userInput)
-		words := strings.Fields(lowerCaseUserInput)
-		fmt.Printf("Your command was: %s\n", words[0])
-	}
+	startRepl()
 }
